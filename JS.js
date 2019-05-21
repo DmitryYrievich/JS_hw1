eaa2fc6fe3bf7da19eb0cd247fb05534cf9f8508
Dmitry_Yrievich <DimaPoxila@gmail.com>
2019-05-21 23:58:45 +0300

    init commit

---
+++ JS.js
@@ -0,0 +1,26 @@
+
+
+/*var userName = prompt('Логин');
+
+if (userName =='Админ') {
+	var pass = prompt('Пароль', '');
+} else {
+	(pass == null);
+}	
+	alert( 'Где пароль?' );*/
+
+
+var userName = prompt('Логин', '');
+
+if (userName =='Админ') {
+
+	var pass = prompt('Пароль', '');
+
+	if (pass == 'Черный властелин') {
+		alert( 'Добро пожаловать' );
+	}	else if (pass == null) {
+		alert( 'Вход отменен' );
+	}	else {
+		alert( 'Пароль не верен' );
+    }
+}    
